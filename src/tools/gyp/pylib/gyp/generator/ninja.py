713f50e7cef818e4dae06ceb345c7f8f93e0079b
HaseenaSainul <h.sainul@metrological.com>
2020-02-06 14:15:51 +0100

    ninja: LD flag reading change reverted back to original

--- src/tools/gyp/pylib/gyp/generator/ninja.py
+++ src/tools/gyp/pylib/gyp/generator/ninja.py
@@ -2116,8 +2116,7 @@ def GenerateOutputForConfig(target_list, target_dicts, data, params,
     master_ninja.variable('cc', cc)
     cxx = GetEnvironFallback(['CXX_target', 'CXX'], cxx)
     master_ninja.variable('cxx', cxx)
-    # Use default value for LD
-    # ld = GetEnvironFallback(['LD_target', 'LD'], ld)
+    ld = GetEnvironFallback(['LD_target', 'LD'], ld)
     rc = GetEnvironFallback(['RC'], 'rc.exe')
 
     if not cc_host:
