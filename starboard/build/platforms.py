eb3d5d758cf29e24526dd8a45fe63cf4638d53d2
Neeraj Deshpande <n.deshpande@metrological.com>
2023-04-24 21:31:59 +0530

    Updated for wpe-arm platform.

--- starboard/build/platforms.py
+++ starboard/build/platforms.py
@@ -42,6 +42,7 @@ PLATFORMS = {
     'evergreen-arm-softfp': 'starboard/evergreen/arm/softfp',
     'evergreen-arm64': 'starboard/evergreen/arm64',
     'win-win32': 'starboard/win/win32',
+    'wpe-arm': 'third_party/starboard/wpe/arm',
 }
 PLATFORMS.update(INTERNAL_PLATFORMS)
 
